181a8a6c44aa74c7516cbf331992d1057d5418cf
Advita Bahl <21JE0063@iitism.ac.in>
2024-09-02 00:56:41 +0530

    final changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,7 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import {
   AiFillGithub,
-  AiOutlineTwitter,
-  AiFillInstagram,
+  AiFillMail,
 } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 
@@ -14,34 +13,45 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
    
-        <Col md="4" className="footer-copywright">
+        <Col md="5" className="footer-copywright">
           <h3>Copyright © {year} AB</h3>
         </Col>
-        <Col md="4" className="footer-body">
-          <ul className="footer-icons">
-            <li className="social-icons">
-              <a
-                href="https://github.com/soumyajit4419"
-                style={{ color: "white" }}
-                target="_blank" 
-                rel="noopener noreferrer"
-              >
-                <AiFillGithub />
-              </a>
-            </li>
-            
-            <li className="social-icons">
-              <a
-                href="https://www.linkedin.com/in/soumyajit4419/"
-                style={{ color: "white" }}
-                target="_blank" 
-                rel="noopener noreferrer"
-              >
-                <FaLinkedinIn />
-              </a>
-            </li>
-            
-          </ul>
+        <Col md="6" className="footer-body">
+        <ul className="footer-icons">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/Advita-bahl"
+                  style={{ color: "white" }}
+                  target="_blank"
+                  rel="noreferrer"
+                  // className="icon-colour  home-social-icons"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.linkedin.com/in/advita-bahl-977876233/"
+                  target="_blank"
+                  rel="noreferrer"
+                  style={{ color: "white" }}
+                  // className="icon-colour  home-social-icons"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="mailto:advitabahl@gmail.com"
+                  target="_blank"
+                  rel="noreferrer"
+                  style={{ color: "white" }}
+                  // className="icon-colour home-social-icons"
+                >
+                  <AiFillMail/>
+                </a>
+              </li>
+            </ul>
         </Col>
       </Row>
     </Container>
